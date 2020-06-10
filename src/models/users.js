f2c7dd705fb280598b3d9d54c533adf8a370fe87
X-Anas-X <anas.zain1996@gmail.com>
2020-06-11 02:06:15 +0300

    need some work

--- src/models/users.js
+++ src/models/users.js
@@ -84,6 +84,44 @@ users.verifyToken = function (token) {
 };
 // *******************************************************\\
 
+let roles = {
+  regular : ['read'],
+  writers : ['read', 'create'],
+  editors: ['read', 'update', 'create'],
+  administrators: ['read', 'update', 'create', 'delete'],
+};
+
+users.permissions = function(capability, role){
+  console.log('check herererer',capability, role);
+  
+
+
+
+  if(role === 'administrators' ){
+    for(let i = 0; i < roles.administrators.length;i++){
+      if(roles.administrators[i]) return true;
+    }
+  }
+  if(role === 'editors' ){
+    for(let i = 0; i < roles.editors.length;i++){
+      if(roles.editors[i]) return true;
+    }
+  }
+  if(role === 'regular' ){
+    for(let i = 0; i < roles.regular.length;i++){
+      if(roles.regular[i]) return true;
+    }
+  }
+  if(role === 'writers' ){
+    for(let i = 0; i < roles.writers.length;i++){
+      if(roles.writers[i]) return true;
+    }
+  }
+  console.log('the roleeeeee', role);
+  
+};
+
+
 
 
 
